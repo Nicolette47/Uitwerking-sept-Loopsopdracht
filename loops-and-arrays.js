4d2a66c2a3678bd1590514895f682faf1768a6b0
nvanzeijl <nvanzeijl@gmail.com>
2024-10-08 18:54:03 +0200

    loops and arrays ook klaar

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -12,8 +12,13 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
+// input : array met namen
+// output: array met namen + je overal achter.
+// Stappenplan: 1. doorloop de array  2. print achter elke naam een -je erachter
 
-
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + 'je');
+}
 
 
 // ==========================================
@@ -30,8 +35,19 @@ const numbers = [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
 
+// input: numbers-array
+// output: alle even getallen * 2 en oneven * 3
+// Stappenplan:
+// 1. bekijk alle items in de array en stel bij ieder item de volgende vraag
+// 2. Is het een even nummer?  Zoja dan * 2    Zonee dan * 3.
 
-
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i] * 2);
+    } else {
+        console.log(numbers[i] * 3);
+    }
+}
 
 
 // ==========================================
@@ -57,5 +73,14 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 11 is 1331'
 // ]
 
+// input: squares array
+// output : squares array waar met het oorspronkelijke getal het volume is berekend een a is ingebed in de zin  "Het volume van [a] is [b]"
+// Stappenplan:
+// 1. Loop door alle nummers van deze array
+// 2. Van elk nummmer in de array moet volume worden berekend via formule lengte * breedte * hoogte
+// 3. het volume-getal moet aan array worden toegevoegd via de zin  "Het volume van [a] is [b]"
 
-
+for ( let i = 0; i < squares.length; i++) {
+    let volume = squares[i];
+    console.log(`Het volume van ${volume} is ${volume * volume * volume}`);
+}
